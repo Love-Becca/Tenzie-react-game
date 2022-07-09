2471a73d5e6aa55a3e179224ef990a3ee367c503
P4ND4 <63761684+Love-Becca@users.noreply.github.com>
2022-07-09 17:53:49 +0100

    all dice now has a unique id

--- tenzie/src/Component/die.js
+++ tenzie/src/Component/die.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './tenzie.css'
-
+//component for the the die number section and some props are passed
 export default function Die(props){
     return(
-        <div className="tenzie-dice">
+        // this conditional statement changes the background color once the isHeld value changes the styles are in css
+        <div className={props.isHeld ? "tenzie-dices": "tenzie-dice"}
+            onClick={props.holdDice}>
             {props.value}
         </div>
     )
--- tenzie/src/Component/tenzie.js
+++ tenzie/src/Component/tenzie.js
@@ -1,25 +1,50 @@
 import React, { useState } from "react";
 import './tenzie.css'
 import Die from "./die";
-
+import {nanoid} from 'nanoid'
 
 export default function TenzieComponent(){
+//This is a state change in which the function generating the random numbers is passed into it such that upon reload we generate random numbers
+    const [diceNumbers, setDiceNumber] = useState(allNewDice());
+//This function randomly generates the numbers ten times and the push an object into the array, the object contains the random number, Id  and isHeld to know if the number has be taken
     function allNewDice(){
         const tenRandomNumbers = []
         for (let i = 0; i < 10; i++) {
-            tenRandomNumbers.push(Math.ceil(Math.random()*6))
+            tenRandomNumbers.push({
+                value:Math.ceil(Math.random()*6),// ceil was used to round it up
+                isHeld:false,
+                id:nanoid() // nanoid are random id generator and it is a dependency installed through npm
+            })
         }
         return tenRandomNumbers
     }
+    //this function gets the particular div or number that is being click
+    function holdDice(id){
+        setDiceNumber((prevDiceNumbers)=>{
+            prevDiceNumbers.map(flip=>({
+            }))
+        })
+    }
+    // we map over each object in the array and return a component in which props are passed such as value to be displayed,each id of each box and the isHeld function 
+    const differentDieNumber = diceNumbers.map(die => 
+        <Die 
+        value ={die.value}
+        key ={die.id}
+        isHeld = {die.isHeld}
+        holdDice = {()=>holdDice(die.id)}// this generate a unique Id 
+        />
+    )
+      // this is an event listener used on the roll button that generate random numbers once the button is clicked
+    function rollDice(){
+        setDiceNumber(allNewDice)
+    }
 
-    const [diceNumbers, setDiceNumber] = useState(allNewDice());
-    const differentDieNumber = diceNumbers.map(die => <Die value ={die}/>)
     return(
         <div className="tenzie-body">
             <h1 className="tenzie-title">Tenzies</h1>
             <p className="tenzie-description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-section">{differentDieNumber}</div>
-            <button className="tenzie-button">Roll</button>
+            <button className="tenzie-button" onClick={rollDice}>Roll</button>
         </div>
     )
 }
